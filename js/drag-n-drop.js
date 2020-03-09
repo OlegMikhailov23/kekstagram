32ac9d6ef72bac81fc55fc3c492554cad022be2a
Oleg Mikhaylov <dkfolkin@gmail.com>
2020-03-09 22:34:33 +0300

    в модуле drag-n-drop применил конструктор

--- js/drag-n-drop.js
+++ js/drag-n-drop.js
@@ -1,23 +1,32 @@
 'use strict';
 
 (function () {
+
+  var Coordinate = function (x) {
+    this._x = x;
+  }
+
+  Coordinate.prototype.setX = function (x) {
+    this._x = x;
+  };
+
   window.dragAndDrop = function (evt, cb1, cb2, el) {
 
     evt.preventDefault();
 
-    var startCoordinate = {
-      x: evt.clientX
-    };
+    var startCoordinate = new Coordinate();
+    startCoordinate.setX(evt.clientX);
 
     var onMouseMove = function (moveEvt) {
       moveEvt.preventDefault();
-      var shift = {
-        x: startCoordinate.x - moveEvt.clientX
-      };
-      startCoordinate = {
-        x: moveEvt.clientX
-      };
-      var position = el.offsetLeft - shift.x;
+
+      var shift = new Coordinate();
+      shift.setX(startCoordinate._x - moveEvt.clientX);
+
+      startCoordinate.setX(moveEvt.clientX);
+      console.log(startCoordinate);
+
+      var position = el.offsetLeft - shift._x;
       cb1(position);
       cb2(position);
     };
